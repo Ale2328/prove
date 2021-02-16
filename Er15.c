659f27039c19e64efba481aed9d83b4f40ad0439
Michele Pagano <75363823+AlmondFlame1287@users.noreply.github.com>
2021-02-16 10:35:52 +0100

    Semplificazione

--- Er15.c
+++ Er15.c
@@ -2,9 +2,9 @@
 	Name: Il gioco del 15
 	Copyright: Nessuno
 	Author: Michele Pagano
-	Date: 15/02/21 21.41
+	Date: 16/02/21 10.31
 	Description: Un semplice gioco formato da 2 giocatori e 11 monete sul tavolo,
-				 l'ultimo a prendere le monete perde.
+	l'ultimo a prendere le monete perde.
 */
 
 #include <stdio.h>
@@ -16,6 +16,7 @@ int monete = 11;
 int moneteRaccolteP, moneteRaccolteM;
 char diff;
 int probInizio, probSconf;
+int contaPlayer, contaMacchina; //implementazione del sistema di conta delle mosse.
 
 //dichiarazione delle funzioni di difficoltà
 void facile();
@@ -24,15 +25,16 @@ void difficile();
 
 //dichiarazione delle funzioni di gioco
 int controlloInput();
-void inizioMacchinaCasuale();
-void inizioMacchinaDeterminato();
-void inizioGiocatoreCasuale();
-void inizioGiocatoreDeterminato();
+void inizioMacchinaC();
+void inizioGiocatoreC();
+void determinato();
+
 
 int main()  //TO DO: ricordarsi di scommentare le varie parti del codice
 {
 	probInizio = rand()%100 + 1;
-
+	probSconf = rand()%100 + 1;
+	
 	printf("Ciao! Seleziona una difficolta'\n");
 	printf("F = facile, M = medio, D = difficile\n");
 	scanf("%c", &diff);
@@ -48,7 +50,7 @@ int main()  //TO DO: ricordarsi di scommentare le varie parti del codice
 			break;
 			
 		case 'd':
-			diffile();
+			difficile();
 			break;
 			
 		default:
@@ -57,96 +59,27 @@ int main()  //TO DO: ricordarsi di scommentare le varie parti del codice
 	}
 }
 
-void facile()
-{
-	srand(time(NULL));
-	probSconf = rand()%100 + 1;
-	
-	if(probInizio >= 80)
-	{
-		if(probSconf > 10)
-			inizioGiocatoreCasuale();
-		else
-//			inizioGiocatoreDeterminato();
-	}
-	else
-	{
-		if(probSconf > 10)
-//			inizioMacchinaCasuale();
-		else
-//			inizioMacchinaDeterminato();
-	}
-	
-}
-
-void medio()
-{
-	srand(time(NULL));
-	probSconf = rand()%100 + 1;
-	
-	if(probInizio >= 50)
-	{
-		if(probSconf > 40)
-			inizioGiocatoreCasuale();
-		else
-//			inizioGiocatoreDeterminato();
-	}
-	else
-	{
-		if(probSconf > 40)
-//			inizioMacchinaCasuale();
-		else
-//			inizioMacchinaDeterminato();
-	}
-	
-}
-
-void difficile()
-{
-	srand(time(NULL));
-	probSconf = rand()%100 + 1;
-	
-	if(probInizio >= 10)
-	{
-		if(probSconf > 75)
-			inizioGiocatoreCasuale();
-		else
-//			inizioGiocatoreDeterminato();
-	}
-	else
-	{
-		if(probSconf > 75)
-//			inizioMacchinaCasuale();
-		else
-//			inizioMacchinaDeterminato();
-	}
-	
-}
-
 int controlloInput(int input)
 {
 	do
 	{
-		printf("Ci sono %d monete. Quante monete vuoi prendere? ", monete);
+		printf("Ci sono %d monete. Quante monete vuoi prendere? \n", monete);
 		scanf("%d", &input);
 		
 		if(input <= 0 && input >= 4)
-			printf("Per favore insersci da un numero da 1 a 3");
-		
-		return input;		
+		{
+			printf("Per favore insersci da un numero da 1 a 3");	
+		}		
 	}
 	while(input <= 0 && input >= 4);
-	
 }
 
-void inizioGiocatoreCasuale()
+void inizioGiocatoreC()
 {
-	srand(time(NULL));
-	int contaPlayer, contaMacchina; //implementazione del sistema di conta delle mosse.
-	
 	while(monete > 0)
-	{
-		controlloInput(moneteRaccolteP);	
+	{	
+		controlloInput(moneteRaccolteP);
+		
 		monete -= moneteRaccolteP;
 		contaPlayer += 1;
 		
@@ -154,16 +87,154 @@ void inizioGiocatoreCasuale()
 		monete -= moneteRaccolteM;
 		contaMacchina += 1;
 		
-		printf("La macchina ha raccolto %d monete. Rimangono %d monete. ", moneteRaccolteM, monete);
+		printf("La macchina ha preso %d monete. Ne rimangono %d\n", moneteRaccolteM, monete);
 	}
-	
-	if(contaMacchina > contaPlayer) //TO DO: verificare il funzionamento dell'efficienza del sistema di conta nella realtà
+}
+
+void inizioMacchinaC()
+{	
+	while(monete > 0)
 	{
-		printf("La macchina ha perso");
+		moneteRaccolteM = rand()%3 + 1;
+		monete -= moneteRaccolteM;
+		contaMacchina += 1;
+		printf("La macchina ha preso %d monete. Ne rimangono %d\n", moneteRaccolteM, monete);
+		
+		controlloInput(moneteRaccolteP);
+		monete -= moneteRaccolteP;
+		contaPlayer += 1;
+	}
+}
+
+void determinato()
+{
+	switch(moneteRaccolteP)
+	{
+		case 1:
+			moneteRaccolteM = 3;
+			monete -= moneteRaccolteM;
+			printf("La macchina ha preso %d monete. Ne rimangono %d\n", moneteRaccolteM, monete);
+			break;
+			
+		case 2:
+			moneteRaccolteM = 2;
+			monete -= moneteRaccolteM;
+			printf("La macchina ha preso %d monete. Ne rimangono %d\n", moneteRaccolteM, monete);
+			break;	
+			
+		case 3:
+			moneteRaccolteM = 1;
+			monete -= moneteRaccolteM;
+			printf("La macchina ha preso %d monete. Ne rimangono %d\n", moneteRaccolteM, monete);
+			break;	
+	}
+}
+
+
+void facile()
+{
+
+		if(probInizio >= 80)
+		{
+			if(probSconf > 10)
+				inizioGiocatoreC();
+			else
+			{
+				while(monete > 0)
+				{
+					controlloInput(moneteRaccolteP);
+					determinato();
+				}
+			}
+		}
+		else
+		{
+			if(probSconf > 10)
+				inizioMacchinaC();
+			else
+			{
+				srand(time(NULL));
+				moneteRaccolteM = rand()%3 + 1;
+				monete -= moneteRaccolteM;
+				printf("La macchina ha preso %d monete. Ne rimangono %d\n", moneteRaccolteM, monete);
+				
+				while(monete > 0)
+				{
+					controlloInput(moneteRaccolteP);
+					determinato();
+				}
+			}
+		}
+}
+
+void medio()
+{
+	if(probInizio >= 50)
+	{
+		if(probSconf > 40)
+			inizioGiocatoreC();
+		else
+		{
+			while(monete > 0)
+			{
+				controlloInput(moneteRaccolteP);
+				determinato();
+			}
+		}
 	}
 	else
 	{
-		printf("La macchina ha vinto");
+		if(probSconf > 40)
+			inizioMacchinaC();
+		else
+		{
+			srand(time(NULL));
+			moneteRaccolteM = rand()%3 + 1;
+			monete -= moneteRaccolteM;
+			printf("La macchina ha preso %d monete. Ne rimangono %d\n", moneteRaccolteM, monete);
+				
+			while(monete > 0)
+			{
+				controlloInput(moneteRaccolteP);
+				determinato();
+			}
+		}
+	}
+	
+}
+
+void difficile()
+{
+	if(probInizio >= 10)
+	{
+		if(probSconf > 75)
+			inizioGiocatoreC();
+		else	
+		{
+			while(monete > 0)
+			{
+				controlloInput(moneteRaccolteP);
+				determinato();
+			}
+		}
+	}
+	else
+	{
+		if(probSconf > 75)
+			inizioMacchinaC();
+		else
+		{
+			srand(time(NULL));
+			moneteRaccolteM = rand()%3 + 1;
+			monete -= moneteRaccolteM;
+			printf("La macchina ha preso %d monete. Ne rimangono %d\n", moneteRaccolteM, monete);
+				
+			while(monete > 0)
+			{
+				controlloInput(moneteRaccolteP);
+				determinato();
+			}
+		}
 	}
 	
 }
